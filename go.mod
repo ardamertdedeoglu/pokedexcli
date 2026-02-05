34b419d22f1dc73dfe274bee92639c06e4ccdcd0
Arda Mert Dedeoğlu <mert.dedeoglu@std.yildiz.edu.tr>
2026-02-05 23:56:52 +0300

    cycling commands with up key support

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ardamertdedeoglu/pokedexcli
 
 go 1.25.6
+
+require (
+	github.com/chzyer/readline v1.5.1 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"log"
+	"strings"
+
+	"github.com/chzyer/readline"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt:          "Pokedex > ",
+		HistoryFile:     "pokedex_history.txt", // Komutlar bu dosyaya kaydedilir
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	for {
-		fmt.Print("Pokedex > ")
-		available := scanner.Scan()
-		if !available {
-			fmt.Println("error getting new token")
-			return
+		command, err := rl.Readline()
+		if err != nil { // Ctrl+C veya Ctrl+D (EOF) durumunda döngüden çıkar
+			break
 		}
-		command := scanner.Text()
+
+		command = strings.TrimSpace(command)
+		if command == "" {
+			continue
+		}
 		clean_command := cleanInput(command)
 		real_command := clean_command[0]
 		res, ok := commands[real_command]
@@ -24,7 +36,7 @@ func main() {
 			fmt.Println("Unknown command")
 		}
 		args := clean_command[1:]
-		err := res.callback(&res.config, args...)
+		err = res.callback(&res.config, args...)
 		if err != nil {
 			fmt.Println(err)
 		}
